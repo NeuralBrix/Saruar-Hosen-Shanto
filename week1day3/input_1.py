4cec7534a1ed76f7594ba6324dc0d7a1cd09a38b
Saruar Hosen <saruarhosen98@gmail.com>
2025-08-18 20:12:28 +0600

    input update

---
+++ week1day3/input_1.py
@@ -0,0 +1,21 @@
+# name=input("Enter your name: ")
+# age=input("Enter your age: ")
+# print(f"Hello, {name}! You are {age} years old.")
+
+# number1=int(input("Enter a number: "))
+# number2=float(input("Enter another number: "))
+# print(f"The sum of {number1} and {number2} is {number1 + number2}.")
+
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
+# print(f"{first_name} {last_name} ")
+
+
+# first_name,last_name= input("Enter your name: ").split(" ")
+# name=(first_name +" "+last_name)
+# print(f"Your full name is: {name}.")
+
+
+first_name,last_name= input("Enter your name: ").split(".")
+name=(first_name +" "+last_name)
+print(f"Your full name is: {name}.")
